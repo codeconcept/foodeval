364963a978ecdb99a25a7680d4a0e92eb651597c
codeconcept <contact@codeconcept.fr>
2017-08-24 14:21:20 +0200

    retrieving articles from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Accueil', component: HomePage },
+      { title: 'Articles préférés', component: ListPage }
     ];
 
   }
